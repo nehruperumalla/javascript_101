875da29648c91a1b2fc068cef9a401ba70f6da7b
nehruperumalla <nehruperumalla1@gmail.com>
2022-03-20 21:59:49 +0530

    feat: Helper Methods

---
+++ app.js
@@ -0,0 +1,3 @@
+document.write('Testing document write') //Write directly onto the webpage
+alert('Alerting...') //Displays text in a pop up
+console.log('Logging to the Browser console..') //Logs the info to the browser's console..
